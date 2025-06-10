7b90600d3df10ebfcdf40f3cf301ae7d1a243bc2
Diana Borja <valeriaborja975@gmail.com>
2025-06-10 09:47:04 -0500

    feat : Carrito de compras Mejorado

--- src/main/java/ec/edu/ups/poo/model/Producto.java
+++ src/main/java/ec/edu/ups/poo/model/Producto.java
@@ -8,44 +8,38 @@ public class Producto {
     public Producto() {
     }
 
-    public Producto(int codigo, double precio, String nombre) {
+    public Producto(int codigo, String nombre, double precio) {
         this.codigo = codigo;
-        this.precio = precio;
         this.nombre = nombre;
-    }
-
-    public int getCodigo() {
-        return codigo;
+        this.precio = precio;
     }
 
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 
     public double getPrecio() {
         return precio;
     }
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-    public void getSubTotal() {
-
-    }
-
     @Override
     public String toString() {
-        return "--Producto--" +
-                "\nCódigo: " + codigo +
-                "\nNombre: " + nombre +
-                "\nPrecio: " + precio ;
+        return nombre + " - $" + precio;
     }
 }
--- src/main/java/ec/edu/ups/poo/servicio/CarritoService.java
+++ src/main/java/ec/edu/ups/poo/servicio/CarritoService.java
@@ -1,9 +1,22 @@
 package ec.edu.ups.poo.servicio;
 
+import ec.edu.ups.poo.model.ItemCarrito;
 import ec.edu.ups.poo.model.Producto;
 
+import java.util.List;
+
 public interface CarritoService {
+
     void agregarProducto(Producto producto, int cantidad);
 
+    void eliminarProducto(int codigoProducto);
+
+    void vaciarCarrito();
+
+    double calcularTotal();
+
+    List<ItemCarrito> obtenerItems();
+
+    boolean estaVacio();
 
 }
